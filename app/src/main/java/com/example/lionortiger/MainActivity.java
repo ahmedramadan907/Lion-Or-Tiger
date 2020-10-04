b89d08ec7d15be4ae6f50d3b37081e180490b9ec
ahmedramadan907 <62665245+ahmedramadan907@users.noreply.github.com>
2020-10-04 23:36:28 +0200

    new feature (no confliction)

--- app/src/main/java/com/example/lionortiger/MainActivity.java
+++ app/src/main/java/com/example/lionortiger/MainActivity.java
@@ -32,10 +32,10 @@ public class MainActivity extends AppCompatActivity {
 
     private player CurrentPlayer = player.ONE;
     private boolean counter = true;
-    private int tag_num;
+    private int tag_num , scoreLion = 0, scoretiger = 0;
 
-    private TextView txt;
-    private Button btn;
+    private TextView txt , txtScore;
+    private Button btn , btnReset;
     private ImageView tappedimage, img;
 
     @Override
@@ -44,7 +44,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         txt = findViewById(R.id.textView2);
+        txtScore = findViewById(R.id.textView3);
         btn = findViewById(R.id.button);
+        btnReset = findViewById(R.id.button2);
 
         choice.clear();
         antiConflict.clear();
@@ -52,29 +54,43 @@ public class MainActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Again();
+            }
+        });
+        txtScore.setText("Lion : 0 \n Tiger : 0");
+
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 Reset();
             }
         });
 
-
     }
+
+
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     public void imageClicked(final View ImageView) {
 
         counter = true;
         tappedimage = (ImageView) ImageView;
+
+
         for (int i = 0; i <= choice.size(); i++) {
 
-            if(Objects.equals(choice.get(i), "tiger") || Objects.equals(choice.get(i), "lion")){
-                if(Integer.parseInt(tappedimage.getTag().toString()) == i){
+            if (choice.get(Integer.parseInt(tappedimage.getTag().toString())) != null) {
+                if (Objects.equals(choice.get(Integer.parseInt(tappedimage.getTag().toString())), "tiger") || Objects.equals(choice.get(Integer.parseInt(tappedimage.getTag().toString())), "lion")) {
+                    for (int key : choice.keySet()) {
 
-                    Toast.makeText(MainActivity.this , "You cannot" , Toast.LENGTH_SHORT).show();
-                    counter = false;
-                    break;
+                        if (Integer.parseInt(tappedimage.getTag().toString()) == key) {
+                            Toast.makeText(MainActivity.this, "You cannot", Toast.LENGTH_SHORT).show();
+                            counter = false;
+                            break;
+                        }
+                    }
+
                 }
-
-
             }
 
         }
@@ -83,16 +99,16 @@ public class MainActivity extends AppCompatActivity {
         if (counter) {
 
             if (CurrentPlayer == player.ONE) {
-                tappedimage.setTranslationX(2000);
-                tappedimage.animate().alpha(1).translationXBy(-2000).rotation(3600).setDuration(2000);
+                tappedimage.setTranslationX(3000);
+                tappedimage.animate().alpha(1).translationXBy(-3000).rotation(1800).setDuration(500);
                 tappedimage.setImageResource(R.drawable.tiger);
                 CurrentPlayer = player.TWO;
                 int tag = Integer.parseInt(tappedimage.getTag().toString());
                 choice.put(tag, "tiger");
 
             } else if (CurrentPlayer == player.TWO) {
-                tappedimage.setTranslationX(2000);
-                tappedimage.animate().alpha(1).translationXBy(-2000).rotation(3600).setDuration(2000);
+                tappedimage.setTranslationX(3000);
+                tappedimage.animate().alpha(1).translationXBy(-3000).rotation(1800).setDuration(500);
                 tappedimage.setImageResource(R.drawable.lion);
                 CurrentPlayer = player.ONE;
                 int tag = Integer.parseInt(tappedimage.getTag().toString());
@@ -113,14 +129,23 @@ public class MainActivity extends AppCompatActivity {
     private void CheckWinner() {
 
 
-//ROW WINNING
+        //ROW WINNING
 
         for (int i = 0; i <= 6; i += 3) {
             if (choice.get(i) != null || choice.get(i + 1) != null || choice.get(i + 2) != null) {
                 if (Objects.equals(choice.get(i), choice.get(i + 1)) && Objects.equals(choice.get(i + 1), choice.get(i + 2))) {
                     txt.setText(choice.get(i) + " is the winner !!");
                     counter = false;
+                    if(choice.get(i) == "lion"){
+                        scoreLion ++ ;
+                        txtScore.setText("Lion : " + String.valueOf(scoreLion));
+                    }else if(choice.get(i) == "tiger"){
+                        scoretiger ++;
+                        txtScore.setText("Tiger : " + String.valueOf(scoretiger));
+                    }
                     break;
+
+
                 }
             }
         }
@@ -132,6 +157,14 @@ public class MainActivity extends AppCompatActivity {
                 if (Objects.equals(choice.get(i), choice.get(i + 3)) && Objects.equals(choice.get(i + 3), choice.get(i + 6))) {
                     txt.setText(choice.get(i) + " is the winner !!");
                     counter = false;
+
+                    if(choice.get(i) == "lion"){
+                        scoreLion ++ ;
+                        txtScore.setText("Lion : " + String.valueOf(scoreLion));
+                    }else if(choice.get(i) == "tiger"){
+                        scoretiger ++;
+                        txtScore.setText("Tiger : " + String.valueOf(scoretiger));
+                    }
                     break;
                 }
             }
@@ -146,6 +179,13 @@ public class MainActivity extends AppCompatActivity {
                 txt.setText(choice.get(i) + " is the winner !!");
                 counter = false;
 
+                if(choice.get(i) == "lion"){
+                    scoreLion ++ ;
+                    txtScore.setText("Lion : " + String.valueOf(scoreLion));
+                }else if(choice.get(i) == "tiger"){
+                    scoretiger ++;
+                    txtScore.setText("Tiger : " + String.valueOf(scoretiger));
+                }
             }
         }
 
@@ -154,6 +194,13 @@ public class MainActivity extends AppCompatActivity {
                 txt.setText(choice.get(j) + " is the winner !!");
                 counter = false;
 
+                if(choice.get(j) == "lion"){
+                    scoreLion ++ ;
+                    txtScore.setText("Lion : " + String.valueOf(scoreLion));
+                }else if(choice.get(j) == "tiger"){
+                    scoretiger ++;
+                    txtScore.setText("Tiger : " + String.valueOf(scoretiger));
+                }
             }
         }
 
@@ -174,6 +221,31 @@ public class MainActivity extends AppCompatActivity {
         choice.clear();
         counter = true;
         CurrentPlayer = player.ONE;
+        scoretiger = 0 ;
+        scoreLion = 0 ;
+        txtScore.setText("Lion : 0\nTiger : 0");
+
+    }
+
+    private void Again() {
+
+        for (int i = 0; i <= arrayTapped.size() - 1; i++) {
+
+            img = findViewById(arrayTapped.get(i));
+
+            img.animate().alpha(0f);
+
+        }
+
+        arrayTapped.clear();
+        txt.setText("");
+        choice.clear();
+        counter = true;
+        CurrentPlayer = player.ONE;
+
+        txtScore.setText("Lion : "+scoreLion +"\nTiger : " +scoretiger);
+
+
     }
 
 
